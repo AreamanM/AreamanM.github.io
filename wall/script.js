59bd1b96085c1bfcc4542e82ba7f8d3bf4c40623
AreamanM <aryaman.marathe@gmail.com>
2026-01-31 04:16:15 +0000

    wall work

--- wall/script.js
+++ wall/script.js
@@ -1,16 +1,31 @@
 // Sample posts
 const posts = [
-  "A mismatch of expectations betweem people leads to a lot of conflicts and problems",
-  "Engineering of any kind is just taking atoms and assembling them into primitive tools which make up contraptions",
-  "Some random shower thought goes here",
+    "Pretty much all relationship problems are due to a mismatch in expectation",
+
+    "Maths, Factorio, Redstone, and programming are all the same things. You build abstractions from builtin primitives and use those abstractions to assemble complex contraptions",
+
+    "Sometimes wanting something too much becomes a detriment towards getting it",
+
+    "There are times when things just don't go your way, and that's okay. There's a lot of value in teaching yourself to let go of what you can't control and focus on the things that are",
+
+    "I think I spend too much time worrying about the future instead of living in the present. There are so many moments between now and tomorrow, and each decides what tomorrow will look like",
+
+    "A lot of complexity in software often comes from not being able to trust your input"
+
 ];
 
-// Render posts
 const wall = document.getElementById("wall");
 
-posts.forEach(text => {
-  const box = document.createElement("div");
-  box.className = "post";
-  box.textContent = text;
-  wall.appendChild(box);
+posts.forEach(post => {
+    const box = document.createElement("div");
+    box.className = "post";
+    box.textContent = post;
+
+    // Create character count element
+    const count = document.createElement("div");
+    count.className = "char-count";
+    count.textContent = post.length + "/280"
+
+    box.appendChild(count);
+    wall.appendChild(box);
 });
